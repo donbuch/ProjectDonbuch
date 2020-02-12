a3158167629574242fae73073cd225e58a0b3a80
Donbuch <39523405+donbuch@users.noreply.github.com>
2020-02-12 19:21:26 +0200

    Add files via upload

--- aladeenhttpd/aladeen.sh
+++ aladeenhttpd/aladeen.sh
@@ -4,11 +4,8 @@ yum install -y httpd
 yum install -y wget
 chkconfig httpd on
 cd /var/www/html
-wget https://raw.githubusercontent.com/linuxacademy/content-aws-csa2019/master/lab_files/03_compute/creating_an_ec2_instance/index.html
-wget https://raw.githubusercontent.com/linuxacademy/content-aws-csa2019/master/lab_files/03_compute/creating_an_ec2_instance/catanimated.gif
-wget https://raw.githubusercontent.com/linuxacademy/content-aws-csa2019/master/lab_files/03_compute/creating_an_ec2_instance/rainbow.gif
-wget https://raw.githubusercontent.com/linuxacademy/content-aws-csa2019/master/lab_files/03_compute/creating_an_ec2_instance/penny.jpeg
-wget https://raw.githubusercontent.com/linuxacademy/content-aws-csa2019/master/lab_files/03_compute/creating_an_ec2_instance/roffle.jpeg
-wget https://raw.githubusercontent.com/linuxacademy/content-aws-csa2019/master/lab_files/03_compute/creating_an_ec2_instance/truffs.jpeg
-wget https://raw.githubusercontent.com/linuxacademy/content-aws-csa2019/master/lab_files/03_compute/creating_an_ec2_instance/winkie.jpeg
+wget https://raw.githubusercontent.com/donbuch/ProjectDonbuch/master/aladeenhttpd/index.html
+wget https://raw.githubusercontent.com/donbuch/ProjectDonbuch/master/aladeenhttpd/aladeen.jpg
+wget https://raw.githubusercontent.com/donbuch/ProjectDonbuch/master/aladeenhttpd/dictatorcut.jpg
+wget https://raw.githubusercontent.com/donbuch/ProjectDonbuch/master/aladeenhttpd/rainbow.gif
 service httpd start
